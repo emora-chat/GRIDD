921ad764a72be9610a695b51c80366384af1ecf8
Sarah Fillwock <sfillwo@emory.edu>
2020-11-19 21:19:21 -0500

    redid grammar of knowledge graph parser (cleaned up and shortened), commit before redoing underlying structure of digraph and concept graph to support multiple of the same label between the same source and target

--- knowledge_graph.py
+++ knowledge_graph.py
@@ -10,85 +10,59 @@ class PredicateTransformer(Transformer):
         self.additions = ConceptGraph()
         self.local_names = {}
 
-    def unnamed_bipredicate(self, args):
-        type, subject, object = args
+    def bipredicate(self, args):
+        name = None
         new_concepts = self.additions.concepts()
-
-        if subject not in self.local_names:
-            if subject not in self.kg_concepts and subject not in new_concepts:
-                raise Exception("subject error - node %s does not exist!" % subject)
-            elif subject not in new_concepts:
-                self.additions.add_node(subject)
+        if len(args) == 4:
+            name, type, subject, object = args
+        elif len(args) == 3:
+            type, subject, object = args
         else:
-            subject = self.local_names[subject]
-
-        if object not in self.local_names:
-            if object not in self.kg_concepts and object not in new_concepts:
-                raise Exception("object error - node %s does not exist!" % object)
-            elif object not in new_concepts:
-                self.additions.add_node(object)
-        else:
-            object = self.local_names[object]
-
-        if type not in self.kg_concepts and type not in new_concepts:
-            raise Exception("predicate_type error - node %s does not exist!" % type)
-        elif type not in new_concepts:
-            self.additions.add_node(type)
-
-        return self.additions.add_bipredicate(subject, object, type)
-
-    def unnamed_monopredicate(self, args):
-        type, subject = args
-        new_concepts = self.additions.concepts()
-
-        if subject not in self.local_names:
-            if subject not in self.kg_concepts and subject not in new_concepts:
-                raise Exception("subject error - node %s does not exist!" % subject)
-            elif subject not in new_concepts:
-                self.additions.add_node(subject)
-        else:
-            subject = self.local_names[subject]
-
-        if type not in self.kg_concepts and type not in new_concepts:
-            raise Exception("predicate_type error - node %s does not exist!" % type)
-        elif type not in new_concepts:
-            self.additions.add_node(type)
-
-        return self.additions.add_monopredicate(subject, type)
-
-    def named_bipredicate(self, args):
-        name, type, subject, object = args
-        new_concepts = self.additions.concepts()
-
-        if subject not in self.local_names:
-            if subject not in self.kg_concepts and subject not in new_concepts:
-                raise Exception("subject error - node %s does not exist!" % subject)
-            elif subject not in new_concepts:
-                self.additions.add_node(subject)
-        else:
-            subject = self.local_names[subject]
-
-        if object not in self.local_names:
-            if object not in self.kg_concepts and object not in new_concepts:
-                raise Exception("object error - node %s does not exist!" % object)
-            elif object not in new_concepts:
-                self.additions.add_node(object)
-        else:
-            object = self.local_names[object]
-
-        if type not in self.kg_concepts and type not in new_concepts:
-            raise Exception("predicate_type error - node %s does not exist!" % type)
-        elif type not in new_concepts:
-            self.additions.add_node(type)
-
+            raise Exception('bipredicate must have 3 or 4 arguments')
+        subject = self._check_subject(subject, new_concepts)
+        object = self._check_object(object, new_concepts)
+        type = self._check_type(type, new_concepts)
         id = self.additions.add_bipredicate(subject, object, type)
-        self.local_names[name] = id
+        if name is not None:
+            self.local_names[name] = id
         return id
 
-    def named_monopredicate(self, args):
-        name, type, subject = args
+    def monopredicate(self, args):
+        name = None
         new_concepts = self.additions.concepts()
+        if len(args) == 3:
+            name, type, subject = args
+        elif len(args) == 2:
+            type, subject = args
+        else:
+            raise Exception('monopredicate must have 2 or 3 arguments')
 
+        subject = self._check_subject(subject, new_concepts)
+        type = self._check_type(type, new_concepts)
+        id = self.additions.add_monopredicate(subject, type)
+        if name is not None:
+            self.local_names[name] = id
+        return id
+
+
+    def instance(self, args):
+        name = None
+        new_concepts = self.additions.concepts()
+        if len(args) == 2:
+            name, type = args
+        elif len(args) == 1:
+            type = args[0]
+        else:
+            raise Exception('instance must have 1 or 2 arguments')
+        id = self.kg._concept_graph.get_next_id()
+        type = self._check_type(type, new_concepts)
+        self.additions.add_node(id)
+        self.additions.add_bipredicate(id, type, 'type')
+        if name is not None:
+            self.local_names[name] = id
+        return id
+
+    def _check_subject(self, subject, new_concepts):
         if subject not in self.local_names:
             if subject not in self.kg_concepts and subject not in new_concepts:
                 raise Exception("subject error - node %s does not exist!" % subject)
@@ -96,44 +70,24 @@ class PredicateTransformer(Transformer):
                 self.additions.add_node(subject)
         else:
             subject = self.local_names[subject]
+        return subject
 
+    def _check_object(self, object, new_concepts):
+        if object not in self.local_names:
+            if object not in self.kg_concepts and object not in new_concepts:
+                raise Exception("object error - node %s does not exist!" % object)
+            elif object not in new_concepts:
+                self.additions.add_node(object)
+        else:
+            object = self.local_names[object]
+        return object
+
+    def _check_type(self, type, new_concepts):
         if type not in self.kg_concepts and type not in new_concepts:
             raise Exception("predicate_type error - node %s does not exist!" % type)
         elif type not in new_concepts:
             self.additions.add_node(type)
-
-        id = self.additions.add_monopredicate(subject, type)
-        self.local_names[name] = id
-        return id
-
-    def named_instance(self, args):
-        name, type = args
-        id = self.kg._concept_graph.get_next_id()
-        new_concepts = self.additions.concepts()
-
-        if type not in self.kg_concepts and type not in new_concepts:
-            raise Exception("predicate_type error - node %s does not exist!" % type)
-        elif type not in new_concepts:
-            self.additions.add_node(type)
-
-        self.additions.add_node(id)
-        self.additions.add_bipredicate(id, type, 'type')
-        self.local_names[name] = id
-        return id
-
-    def unnamed_instance(self, args):
-        type = args[0]
-        id = self.kg._concept_graph.get_next_id()
-        new_concepts = self.additions.concepts()
-
-        if type not in self.kg_concepts and type not in new_concepts:
-            raise Exception("predicate_type error - node %s does not exist!" % type)
-        elif type not in new_concepts:
-            self.additions.add_node(type)
-
-        self.additions.add_node(id)
-        self.additions.add_bipredicate(id, type, 'type')
-        return id
+        return type
 
     def name(self, args):
         return str(args[0])
@@ -154,18 +108,31 @@ class KnowledgeGraph:
 
     def __init__(self, filename=None, nodes=None):
         self._concept_graph = ConceptGraph(nodes=nodes)
+        # self._grammar = r"""
+        #     start: (unnamed_bipredicate | named_bipredicate | unnamed_monopredicate | named_monopredicate)+
+        #     unnamed_bipredicate: type "(" subject "," object ")"
+        #     unnamed_monopredicate: type "(" subject ")"
+        #     named_bipredicate: name "/" type "(" subject "," object ")"
+        #     named_monopredicate: name "/" type "(" subject ")"
+        #     name: STRING
+        #     type: STRING
+        #     subject: STRING | (unnamed_bipredicate | named_bipredicate | unnamed_monopredicate | named_monopredicate) | (unnamed_instance | named_instance)
+        #     object: STRING | (unnamed_bipredicate | named_bipredicate | unnamed_monopredicate | named_monopredicate) | (unnamed_instance | named_instance)
+        #     unnamed_instance: type "(" ")"
+        #     named_instance: name "/"  type "(" ")"
+        #     STRING: /[a-z_A-Z0-9]/+
+        #     WHITESPACE: (" " | "\n")+
+        #     %ignore WHITESPACE
+        # """
         self._grammar = r"""
-            start: (unnamed_bipredicate | named_bipredicate | unnamed_monopredicate | named_monopredicate)+
-            unnamed_bipredicate: type "(" subject "," object ")"
-            unnamed_monopredicate: type "(" subject ")"
-            named_bipredicate: name "/" type "(" subject "," object ")" 
-            named_monopredicate: name "/" type "(" subject ")"
+            start: (bipredicate | monopredicate)+
+            bipredicate: (name "/")? type "(" subject "," object ")"
+            monopredicate: (name "/")? type "(" subject ")"
+            instance: (name "/")? type "(" ")"
             name: STRING
             type: STRING 
-            subject: STRING | (unnamed_bipredicate | named_bipredicate | unnamed_monopredicate | named_monopredicate) | (unnamed_instance | named_instance)
-            object: STRING | (unnamed_bipredicate | named_bipredicate | unnamed_monopredicate | named_monopredicate) | (unnamed_instance | named_instance)
-            unnamed_instance: type "(" ")"
-            named_instance: name "/"  type "(" ")"
+            subject: STRING | bipredicate | monopredicate | instance
+            object: STRING | bipredicate | monopredicate | instance
             STRING: /[a-z_A-Z0-9]/+
             WHITESPACE: (" " | "\n")+
             %ignore WHITESPACE
@@ -238,7 +205,9 @@ if __name__ == '__main__':
 
     text = """
         gps/go(person,store)
+        buy(person,store)
         reason(happy,gps)
+        t/time(person)
     """
 
     kg = KnowledgeGraph(nodes=['person','store','icecream',
