1e6524241cfbe92dbdeb7306fd7cfd4e2385a4a0
James Finch <jdfinch@emory.edu>
2021-01-13 21:12:07 -0500

    Score graph built and tested

--- data_structures/score_graph.py
+++ data_structures/score_graph.py
@@ -1,10 +1,56 @@
 
-from structpy.graph.directed.labeled.multilabeled_parallel_digraph_networkx import MultiLabeledParallelDigraphNX as Graph
+from data_structures.score_graph_spec import ScoreGraphSpec
+from structpy.graph.directed.labeled.data.multilabeled_digraph_data import MultiLabeledDigraphDataNX as Graph
+from collections import defaultdict
 
 
 class ScoreGraph(Graph):
 
-    def __init__(self, edges=None, updater_dict=None, get_fn=None, set_fn=None, default=0):
+    def __init__(self, edges=None, updaters=None, get_fn=None, set_fn=None, default=0):
         Graph.__init__(self, edges=edges)
+        self._get_fn = get_fn
+        self._set_fn = set_fn
+        self.updaters = {}
+        if updaters is not None:
+            for label, updater in updaters.items():
+                self.set_updater(label, updater)
+        self._default = float(default)
+        if self._get_fn is not None:
+            for node in self.nodes():
+                self.data(node).score = self._get_fn(node)
+        else:
+            for node in self.nodes():
+                self.data(node).score = 0
+
+    def score(self, node):
+        return self.data(node).score
+
+    def push(self):
+        for node in self.nodes():
+            self._set_fn(node, self.data(node).score)
+
+    def pull(self):
+        for node in self.nodes():
+            self.data(node).score = self._get_fn(node)
+
+    def update(self, iterations=1, pull=False, push=False, lr=1):
+        if pull:
+            self.pull()
+        for i in range(iterations):
+            deltas = defaultdict(float)
+            for s, t, l in self.edges():
+                if l in self.updaters:
+                    s_delta, t_delta = self.updaters[l](self.score(s), self.score(t))
+                    deltas[s] += s_delta * lr
+                    deltas[t] += t_delta * lr
+            for node, delta in deltas.items():
+                self.data(node).score += delta
+        if push:
+            self.push()
+
+    def set_updater(self, label, updater_fn):
+        self.updaters[label] = updater_fn
 
 
+if __name__ == '__main__':
+    print(ScoreGraphSpec.verify(ScoreGraph))
--- data_structures/score_graph_spec.py
+++ data_structures/score_graph_spec.py
@@ -3,9 +3,19 @@ from structpy import specification
 from structpy.graph.directed.labeled.data.multilabeled_digraph_data_spec import MultiLabeledDigraphDataSpec
 
 
-E = 0.001
+E = 0.0001
 
 my_values = {'a': 2, 'b': 8, 'c': 3, 'd': 6}
+
+def push(a, b):
+    num = (b - a)
+    denom = (b - a) ** 2
+    bdelta = num / denom
+    adelta = 0
+    return adelta, bdelta
+
+def pull(a, b):
+    pass
 
 @specification
 class ScoreGraphSpec:
@@ -14,46 +24,63 @@ class ScoreGraphSpec:
     """
 
     @specification.init
-    def SCOREGRAPH(ScoreGraph, edges=None, updater_dict=None, get_fn=None, set_fn=None, default=0):
+    def SCOREGRAPH(ScoreGraph, edges=None, updaters=None, get_fn=None, set_fn=None, default=0):
         """
         Create a ScoreGraph.
         """
         score_graph = ScoreGraph(
             edges=[
-            ('a', 'b', 'push'),
-            ('c', 'b', 'push'),
-            ('b', 'd', 'pull')],
-            updater_dict={
-                'push': (lambda x, y: (x, y + (y - x) / (y - x) ** 2 if y != x else y)),
-                'pull': (lambda x, y: (x, y - (y - x) / (y - x) ** 2 if y != x else y))
+            ('a', 'c', 'push'),
+            ('b', 'c', 'push'),
+            ('c', 'd', 'pull')],
+            updaters={
+                'push': (lambda x, y: (0, (y - x) / (y - x) ** 2 if y != x else 0)),
+                'pull': (lambda x, y: (0, (x - y)))
             },
             get_fn=lambda x: my_values.get(x),
-            set_fn=lambda x, y: my_values.setdefault(x, y)
+            set_fn=lambda x, y: my_values.__setitem__(x, y)
         )
         return score_graph
+
+    def score(score_graph, node):
+        """
+        Get the score (float) of a given node.
+        """
+        assert score_graph.score('a') == 2
+        assert score_graph.score('b') == 8
 
     def update(score_graph, iterations=1, pull=False, push=False):
         """
 
         """
         score_graph.update()
-        assert score_graph.node_data('b', 'value') == None
-
+        c_score = score_graph.score('c')
+        assert c_score == 3.8
+        score_graph.update(1000, lr=0.1)
+        c_score = score_graph.score('c')
+        assert 5 - E < c_score < 5 + E
 
     def push(score_graph):
         """
 
         """
-        pass
+        score_graph.push()
+        assert 5 - E < my_values['c'] < 5 + E
+        assert 5 - E < my_values['d'] < 5 + E
+        assert my_values['a'] == 2
 
     def pull(score_graph):
         """
 
         """
-        pass
+        my_values['d'] = 10
+        score_graph.pull()
+        assert score_graph.score('d') == 10
 
-    def set_update_function(score_graph, label, resolution_function):
+    def set_updater(score_graph, label, updater_fn):
         """
 
         """
-        pass
+        score_graph.set_updater('pull', lambda x, y: (0, 12-y))
+        score_graph.update()
+        assert score_graph.score('d') == 12
