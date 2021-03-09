b1c2d17ad6b4dd84b18b46488785a0b20397c669
Sarah Fillwock <sfillwo@emory.edu>
2021-03-09 14:19:39 -0500

    fixed comment error

--- data_structures/knowledge_parser_spec.py
+++ data_structures/knowledge_parser_spec.py
@@ -52,6 +52,8 @@ class KnowledgeParserSpec:
             fido=dog()
             bark(fido)
             ;
+            
+            /* comment */
         '''
         concept_graph = KnowledgeParser.from_data(ls1, namespace='cg_')
         assert concept_graph.has('fluffy', 'type', 'cat')
