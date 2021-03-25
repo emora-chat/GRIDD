5b1ba41cf1a8051cba32d46ca261d437aaad2720
James Finch <jdfinch@emory.edu>
2021-03-24 23:19:46 -0400

    New parser complete with initial testing

--- data_structures/new_knowledge_parser_spec.py
+++ data_structures/new_knowledge_parser_spec.py
@@ -7,15 +7,25 @@ class KnowledgeParserSpec:
 
     @specification.init
     def KNOWLEDGE_PARSER(KnowledgeParser):
-        parser = KnowledgeParser()
+        parser = KnowledgeParser(set(), {'predicate', 'object', 'type', 'expression'}, {'predicate', 'type'})
         return parser
 
     def parse(parser, logic_string):
         test = '''
-        
+        entity = (object)
         animal = (entity)
         pet = (entity)
-        dog = (animal, pet);
+        dog = (animal, pet)
+        person = (entity)
+        group = (entity)
+        something = (entity)
+        ;
+        
+        mary=person()
+        chase = (predicate)
+        expr = (predicate)
+        [excited, happy, heppy] = (predicate)
+        [reason, members] = (predicate)
         
         [amphibian, reptile, bird] = (animal, pet) /* this is a comment */
         
@@ -23,7 +33,7 @@ class KnowledgeParserSpec:
         /* Here is another comment */
         fido/dog(){"name": "fido", "attrs": {"x": [2, 3], "y": true}}
         
-        c/chase(fido, fluffy=pet())
+        c=chase(fido, fluffy=pet())
         
         expr("fido, doggo, puppy dog, pooch", fido)
         expr(["fluffy", "fluffers"], fluffy)
