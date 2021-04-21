071a0b67e016ffbf27239e1835c3df3d401ae536
Sarah Fillwock <sfillwo@emory.edu>
2021-04-21 18:04:26 -0400

    templatized nlg added; need to crate compiler for template rules still

--- modules/responsegen_by_templates.py
+++ modules/responsegen_by_templates.py
@@ -1,7 +1,19 @@
 from GRIDD.modules.responsegen_by_templates_spec import ResponseTemplatesSpec
-from GRIDD.data_structures.concept_compiler import TemplateConceptCompiler
+# from GRIDD.data_structures.concept_compiler import TemplateConceptCompiler
 from GRIDD.data_structures.concept_graph import ConceptGraph
 from GRIDD.utilities.utilities import collect
+from GRIDD.globals import *
+import time
+
+from simplenlg.framework.NLGFactory         import *
+from simplenlg.realiser.english.Realiser    import *
+from simplenlg.lexicon.Lexicon              import *
+from simplenlg.phrasespec.VPPhraseSpec      import *
+
+from simplenlg.features.NumberAgreement     import *
+from simplenlg.features.Feature             import *
+from simplenlg.features.Tense               import *
+from simplenlg.features.Person              import *
 
 """
 These two classes work in conjunction with the service-implementation of the InferenceEngine
@@ -14,84 +26,184 @@ Execution Sequence:
     ResponseTemplateFiller
 """
 
-class ResponseTemplateFinder:
-    """
-    Loads the template rules from template_dir upon initialization and returns them when called.
-    """
-
-    def __init__(self, template_dir):
-        compiler = TemplateConceptCompiler(predicates=None, types=None, namespace='c_')
-        predicates, metalinks, metadatas = compiler.compile(collect(template_dir))
-        template_cg = ConceptGraph(predicates, metalinks=metalinks, metadata=metadatas,
-                                      namespace='t_')
-        self.template_rules = template_cg.rules()
-
-    def __call__(self):
-        return self.template_rules
+# class ResponseTemplateFinder:
+#     """
+#     Loads the template rules from template_dir upon initialization and returns them when called.
+#     """
+#
+#     def __init__(self, template_dir):
+#         compiler = TemplateConceptCompiler(predicates=None, types=None, namespace='c_')
+#         predicates, metalinks, metadatas = compiler.compile(collect(template_dir))
+#         template_cg = ConceptGraph(predicates, metalinks=metalinks, metadata=metadatas,
+#                                       namespace='t_')
+#         self.template_rules = template_cg.rules()
+#
+#     def __call__(self):
+#         return self.template_rules
 
 class ResponseTemplateFiller:
     """
     Fills out a template based on the provided variable matches and concept expressions.
     """
 
-    def __call__(self, matches, wm):
+    def __init__(self):
+        self.lexicon = Lexicon.getDefaultLexicon()
+        self.nlgFactory = NLGFactory(self.lexicon)
+        self.realiser = Realiser(self.lexicon)
+
+    def __call__(self, matches, cg):
         expr_dict = {}
-        for s, t, o, i in wm.predicates(predicate_type='expr'):
+        for s, t, o, i in cg.predicates(predicate_type='expr'):
             if o not in expr_dict:
                 expr_dict[o] = s.replace('"', '')
         candidates = []
         for match_dict, string_spec_ls in matches:
-            candidates.append(self.fill_string(match_dict, expr_dict, string_spec_ls))
+            candidates.append((match_dict, self.fill_string(match_dict, expr_dict, string_spec_ls, cg)))
         return candidates
 
-    def fill_string(self, match_dict, expr_dict, string_spec_ls):
-        fillers = {}
-        for e in string_spec_ls:
-            if not isinstance(e, tuple) and e not in fillers and e.isupper():
-                fillers[e] = expr_dict[match_dict[e]]
+    def fill_string(self, match_dict, expr_dict, string_spec_ls, cg):
+        realizations = {}
+        specifications = {}
 
+        # Replacement of parameter-less variables
         for e in string_spec_ls:
-            if isinstance(e, tuple) and str(e) not in fillers:
+            if not isinstance(e, tuple) and e not in realizations and e.isupper():
+                match = match_dict[e]
+                if match in expr_dict: # the matched concept for the variable is a named concept
+                    realizations[e] = expr_dict[match_dict[e]]
+                else: # not a named concept
+                    np = self.nlgFactory.createNounPhrase()
+                    # need to get main type
+                    types = cg.types(match)
+                    noun = self.nlgFactory.createNLGElement(list(types - {match, GROUP})[0], LexicalCategory.NOUN)
+                    # determine whether reference or instance
+                    if cg.metagraph.out_edges(match, REF) or list(cg.predicates(match, USER_AWARE)):
+                        np.setDeterminer('the')
+                    else:
+                        np.setDeterminer('a')
+                    # whether group
+                    if GROUP in types:
+                        noun.setFeature(Feature.NUMBER, NumberAgreement.PLURAL)
+                    else:
+                        noun.setFeature(Feature.NUMBER, NumberAgreement.SINGULAR)
+                    np.setNoun(noun)
+                    realizations[e] = self.realiser.realiseSentence(np)[:-1]
+                    specifications[e] = np
+
+        # Replacement of parameterized variables
+        for e in string_spec_ls:
+            if isinstance(e, tuple) and str(e) not in realizations:
                 surface_form, spec = e
+                clause = self.nlgFactory.createClause()
+                to_remove = set()
                 if 't' in spec:
-                    surface_form += '.' + spec['t']
-                if 'p' in spec:
-                    surface_form += '.' + spec['p']
-                if 'f' in spec:
-                    surface_form += '.' + spec['f']
+                    clause.setVerb(surface_form)
+                    if spec['t'] == 'past':
+                        clause.setFeature(Feature.TENSE, Tense.PAST)
+                    elif spec['t'] == 'present':
+                        clause.setFeature(Feature.TENSE, Tense.PRESENT)
+                    elif spec['t'] == 'future':
+                        clause.setFeature(Feature.TENSE, Tense.FUTURE)
+                    else:
+                        print('WARNING! You specified an nlg `tense` parameter that is not handled (%s).'%spec['t'])
                 if 's' in spec:
-                    surface_form += '.' + fillers.get(spec['s'], spec['s'])
-                if 'o' in spec:
-                    surface_form += '.' + fillers.get(spec['o'], spec['o'])
-                fillers[str(e)] = surface_form
+                    subject = realizations.get(spec['s'], spec['s'])
+                    clause.setSubject(subject)
+                    if spec['s'] in specifications:
+                        clause.setFeature(Feature.NUMBER, specifications[spec['s']].features['number'])
+                    to_remove.add(subject)
+                    # todo - determine if we need to use simplenlg to determine plurality/person?
+                sentence = self.realiser.realiseSentence(clause)
+                for r in to_remove:
+                    sentence = sentence.replace(r, '')
+                realizations[str(e)] = sentence[:-1].strip()
 
-        final_str = [fillers.get(str(e), str(e)) for e in string_spec_ls]
+        final_str = [realizations.get(str(e), str(e)) for e in string_spec_ls]
         return ' '.join(final_str)
 
+    def test(self):
+
+        # Plurality & Determiner Object Agreement
+
+        s = time.time()
+        np = self.nlgFactory.createNounPhrase()
+        np.setDeterminer('the')
+        noun = self.nlgFactory.createNLGElement('cat', LexicalCategory.NOUN)
+        noun.setFeature(Feature.NUMBER, NumberAgreement.SINGULAR)
+        np.setNoun(noun)
+        print('[%.2f] %s'%(time.time() - s, self.realiser.realiseSentence(np)))
+
+        s = time.time()
+        np = self.nlgFactory.createNounPhrase()
+        np.setDeterminer('the')
+        noun = self.nlgFactory.createNLGElement('cat', LexicalCategory.NOUN)
+        noun.setFeature(Feature.NUMBER, NumberAgreement.PLURAL)
+        np.setNoun(noun)
+        print('[%.2f] %s'%(time.time() - s, self.realiser.realiseSentence(np)))
+
+        s = time.time()
+        np = self.nlgFactory.createNounPhrase()
+        np.setDeterminer('a')
+        noun = self.nlgFactory.createNLGElement('cat', LexicalCategory.NOUN)
+        noun.setFeature(Feature.NUMBER, NumberAgreement.SINGULAR)
+        np.setNoun(noun)
+        print('[%.2f] %s'%(time.time() - s, self.realiser.realiseSentence(np)))
+
+        s = time.time()
+        np = self.nlgFactory.createNounPhrase()
+        np.setDeterminer('a')
+        noun = self.nlgFactory.createNLGElement('cat', LexicalCategory.NOUN)
+        noun.setFeature(Feature.NUMBER, NumberAgreement.PLURAL)
+        np.setNoun(noun)
+        print('[%.2f] %s'%(time.time() - s, self.realiser.realiseSentence(np)))
+
+        # Verb Tense and Subject Person Agreement
+
+        s = time.time()
+        clause = self.nlgFactory.createClause()
+        clause.setSubject('Mark')
+        clause.setVerb('sing')
+        clause.setFeature(Feature.TENSE, Tense.PRESENT)
+        print('[%.2f] %s' % (time.time() - s, self.realiser.realiseSentence(clause)))
+
+        s = time.time()
+        clause = self.nlgFactory.createClause()
+        clause.setSubject('Mark')
+        clause.setVerb('sing')
+        clause.setFeature(Feature.TENSE, Tense.PAST)
+        print('[%.2f] %s' % (time.time() - s, self.realiser.realiseSentence(clause)))
+
+        s = time.time()
+        clause = self.nlgFactory.createClause()
+        clause.setSubject('Mark')
+        clause.setVerb('sing')
+        clause.setFeature(Feature.TENSE, Tense.FUTURE)
+        print('[%.2f] %s' % (time.time() - s, self.realiser.realiseSentence(clause)))
+
+        s = time.time()
+        clause = self.nlgFactory.createClause()
+        clause.setSubject('I')
+        clause.setVerb('sing')
+        clause.setFeature(Feature.TENSE, Tense.PRESENT)
+        print('[%.2f] %s' % (time.time() - s, self.realiser.realiseSentence(clause)))
+
+        s = time.time()
+        clause = self.nlgFactory.createClause()
+        clause.setSubject('I')
+        clause.setVerb('sing')
+        clause.setFeature(Feature.TENSE, Tense.PAST)
+        print('[%.2f] %s' % (time.time() - s, self.realiser.realiseSentence(clause)))
+
+        s = time.time()
+        clause = self.nlgFactory.createClause()
+        clause.setSubject('I')
+        clause.setVerb('sing')
+        clause.setFeature(Feature.TENSE, Tense.FUTURE)
+        print('[%.2f] %s' % (time.time() - s, self.realiser.realiseSentence(clause)))
 
 
-"""
-spec = re.match(r"(.*)\([tpfso]=.*\)", e)
-if spec:
-    surface_form = spec.groups()[0]
-    fillers[e] = surface_form
-    t = re.match(r"t=([^,)]*)", e)
-    if t:
-        t = t.groups()[0]
-        fillers[e] += '.' + t
-    p = re.match(r"p=([^,)]*)", e)
-    if p:
-        p = p.groups()[0]
-    f = re.match(r"f=([^,)]*)", e)
-    if f:
-        f = f.groups()[0]
-    s = re.match(r"s=([^,)]*)", e)
-    if s:
-        s = s.groups()[0]
-    o = re.match(r"o=([^,)]*)", e)
-    if o:
-        o = o.groups()[0]
-"""
+
+
 
 
 
@@ -99,3 +211,19 @@ if spec:
 
 if __name__ == '__main__':
     print(ResponseTemplatesSpec.verify(ResponseTemplateFiller))
+
+    # from os.path import join
+    # from GRIDD.data_structures.inference_engine import InferenceEngine
+    #
+    # tfind = ResponseTemplateFinder(join('GRIDD', 'resources', 'nlg_templates'))
+    # infer = InferenceEngine()
+    #
+    # logic = '''
+    # '''
+    # cg = ConceptGraph(namespace='wm')
+    # ConceptGraph.construct(cg, logic)
+
+    # tfill = ResponseTemplateFiller()
+    # tfill.test()
+
+
--- modules/responsegen_by_templates_spec.py
+++ modules/responsegen_by_templates_spec.py
@@ -1,37 +1,52 @@
 from structpy import specification
 from GRIDD.data_structures.concept_graph import ConceptGraph
-
+from GRIDD.globals import *
 
 @specification
 class ResponseTemplatesSpec:
 
     @specification.init
     def ResponseTemplateFiller(ResponseTemplateFiller):
-        response_templates = ResponseTemplateFiller()
-        return response_templates
+        response_template_filler = ResponseTemplateFiller()
+        return response_template_filler
 
-    def fill_string(response_templates, match_dict, expr_dict, string_spec_ls):
+    def __call__(self, matches, wm):
+        """
+        Fills the corresponding template for each match found
+
+        `matches` - is a list of (match_dict, string_spec_ls) where match_dict maps
+        variables from the rule to matches in the concept_graph and string_spec_ls
+        is a list of string elements that contain surface form realization parameters
+        and form the full response when properly handled and concatenated.
+        """
+        pass
+
+    def fill_string(response_template_filler, match_dict, expr_dict, string_spec_ls, wm):
         """
         Replace variables in string_spec_ls with expressions of matches found in match_dict and
         surface forms with grammatical realizations.
 
         Grammatical markers are:
-            t - discrete tense category (past, present)
-            p - binary plurality        (True, False)
-            f - discrete form category  (gerund, infinitive)
+            t - discrete tense category (past, present, future)
             s - subject dependency for agreement between subjects and verbs
-            o - object dependency for agreement between determiners and objects
+
+        An appropriate determiner and its agreement with its object is derived from logical form
+        and as such is not specified in the template.
         """
+        cg = ConceptGraph()
+        ConceptGraph.construct(cg, 'time(hike(emora), now)')
         match_dict = {
             'X': 'emora'
         }
         expr_dict = {
-            'emora': 'i'
+            'emora': 'I'
         }
         string_spec_ls = ['X', ('hike',{'t': 'present', 's': 'X'}), '.']
-        filled = response_templates.fill_string(match_dict, expr_dict, string_spec_ls)
-        assert filled == 'I hike.'
+        filled = response_template_filler.fill_string(match_dict, expr_dict, string_spec_ls, cg)
+        assert filled == 'I hike .'
 
+        cg = ConceptGraph()
+        ConceptGraph.construct(cg, 'time(hike(Mary), now)')
         match_dict = {
             'X': 'Mary'
         }
@@ -39,5 +54,45 @@ class ResponseTemplatesSpec:
             'Mary': 'Mary'
         }
         string_spec_ls = ['X', ('hike',{'t': 'present', 's': 'X'}), '.']
-        filled = response_templates.fill_string(match_dict, expr_dict, string_spec_ls)
-        assert filled == 'Mary hikes.'
+        filled = response_template_filler.fill_string(match_dict, expr_dict, string_spec_ls, cg)
+        assert filled == 'Mary hikes .'
+
+        cg = ConceptGraph()
+        ConceptGraph.construct(cg, 'time(cute(c=cat()), now)')
+        match_dict = {
+            'X': 'c'
+        }
+        string_spec_ls = ['X', ('be',{'t': 'present', 's': 'X'}), 'cute', '.']
+        filled = response_template_filler.fill_string(match_dict, expr_dict, string_spec_ls, cg)
+        assert filled == 'A cat is cute .'
+
+        cg = ConceptGraph()
+        ConceptGraph.construct(cg, f'time(cute(c=cat()), now) {USER_AWARE}(c)')
+        ((s,t,o,i),) = cg.predicates('c', 'type', 'cat')
+        cg.add(i, USER_AWARE)
+        match_dict = {
+            'X': 'c'
+        }
+        string_spec_ls = ['X', ('be',{'t': 'present', 's': 'X'}), 'cute', '.']
+        filled = response_template_filler.fill_string(match_dict, expr_dict, string_spec_ls, cg)
+        assert filled == 'The cat is cute .'
+
+        cg = ConceptGraph()
+        ConceptGraph.construct(cg, 'time(cute(c=cat()), now) type(c, group)')
+        match_dict = {
+            'X': 'c'
+        }
+        string_spec_ls = ['X', ('be',{'t': 'present', 's': 'X'}), 'cute', '.']
+        filled = response_template_filler.fill_string(match_dict, expr_dict, string_spec_ls, cg)
+        assert filled == 'Some cats are cute .'
+
+        cg = ConceptGraph()
+        ConceptGraph.construct(cg, f'time(cute(c=cat()), now) t=type(c, group) {USER_AWARE}(c) {USER_AWARE}(t)')
+        ((s,t,o,i),) = cg.predicates('c', 'type', 'cat')
+        cg.add(i, USER_AWARE)
+        match_dict = {
+            'X': 'c'
+        }
+        string_spec_ls = ['X', ('be',{'t': 'present', 's': 'X'}), 'cute', '.']
+        filled = response_template_filler.fill_string(match_dict, expr_dict, string_spec_ls, cg)
+        assert filled == 'The cats are cute .'
